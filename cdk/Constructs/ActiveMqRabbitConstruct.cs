ff160681beef4647f337b7ba2ae27e7ace0a94bd
Carlos Pons <karlospn@gmail.com>
2022-01-22 16:31:35 +0100

    Add activemq rabbit construct to cdk stack

--- cdk/Constructs/ActiveMqRabbitConstruct.cs
+++ cdk/Constructs/ActiveMqRabbitConstruct.cs
@@ -1,4 +1,6 @@
-﻿using Amazon.CDK.AWS.EC2;
+﻿using System.Linq;
+using Amazon.CDK.AWS.AmazonMQ;
+using Amazon.CDK.AWS.EC2;
 using Constructs;
 
 namespace Aws.Otel.Cdk.Stack.Constructs
@@ -8,6 +10,29 @@ namespace Aws.Otel.Cdk.Stack.Constructs
         public ActiveMqRabbitConstruct(Construct scope, string id, Vpc vpc) 
             : base(scope, id)
         {
+            var subnetIds = vpc.PublicSubnets
+                .Select(x => x.SubnetId)
+                .ToArray();
+
+            _ = new CfnBroker(this, 
+                "aws-otel-tracing-demo-rabbit-cluster", 
+                new CfnBrokerProps
+            {
+                BrokerName = "aws-otel-tracing-demo-rabbit-cluster",
+                EngineType = "RABBITMQ",
+                PubliclyAccessible = true,
+                Users = new[] { new CfnBroker.UserProperty {
+                    Password = "specialguest",
+                    Username = "P@ssw0rd111!",
+                    ConsoleAccess = true,
+                    Groups = new [] { "administrator" }
+                }},
+                AutoMinorVersionUpgrade = false,
+                DeploymentMode = "SINGLE_INSTANCE",
+                EngineVersion = "3.8.6",
+                HostInstanceType = "mq.t3.micro",
+                SubnetIds = subnetIds
+            });
 
         }
     }
